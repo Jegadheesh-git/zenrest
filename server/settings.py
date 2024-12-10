501c1f6d329c465d9cb519d02b6752849f5cfe72
Jegadheesh P <jegadheesh.mail@gmail.com>
2024-12-10 13:15:11 +0530

    Cors testing

--- server/settings.py
+++ server/settings.py
@@ -151,6 +151,8 @@ CORS_ALLOWED_ORIGINS = [
     'http://localhost:5173',
     'http://127.0.0.1:8000',
     'http://13.201.19.162',
+    'https://zensportstech.com',
+    'https://www.zensportstech.com',
     
 ]
 
